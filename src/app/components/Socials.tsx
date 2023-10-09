de7d07868ac58f9f683b45ffcbae3b5f8ec4e2b1
Pablo <ezekielgonzalezr@gmail.com>
2023-10-09 11:54:32 -0600

    Build social navbar

---
+++ src/app/components/Socials.tsx
@@ -0,0 +1,36 @@
+import { NextPage } from 'next';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { SiWellfound, SiGmail } from 'react-icons/si';
+
+interface Props {}
+
+const Socials: NextPage<Props> = ({}) => {
+  return (
+    <div className='bg-purple rounded'>
+      <ul className='flex-col p-2'>
+        <li className='m-4 text-2xl text-white hover:text-secondary hover:scale-110'>
+          <a href='https://github.com/PabloEGonz'>
+            <BsGithub />
+          </a>
+        </li>
+        <li className='m-4 text-2xl text-white hover:text-secondary hover:scale-110'>
+          <a href='https://www.linkedin.com/in/pabloezekielgonz/'>
+            <BsLinkedin />
+          </a>
+        </li>
+        <li className='m-4 text-3xl text-white hover:text-secondary hover:scale-110'>
+          <a href='https://wellfound.com/u/pabloezekielgonz'>
+            <SiWellfound />
+          </a>
+        </li>
+        <li className='m-4 text-2xl text-white hover:text-secondary hover:scale-110'>
+          <a href='mailto:ezekielgonzalezr@gmail.com'>
+            <SiGmail />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Socials;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
+import Socials from "./components/Socials";
 export default function Home() {
   return (
     <>
       <main className='flex min-h-screen flex-col items-center justify-between p-24'>
         <section>
-          <h1 className='mb-3 text-6xl text-primary'>
-            Hey there I'm Ezequiel
-          </h1>
-          <h2 className='text-secondary text-4xl'>
+          <h1 className='mb-5 text-6xl text-primary'>Hey there I'm Ezequiel</h1>
+          <h2 className='mb-6 bg-primary text-4xl text-secondary'>
             I'm a full-stack software developer
           </h2>
+          <p className='w-4/6'>
+            Full-Stack Developer, Ruby is my go-to programming language,
+            trilingual, Stack: JavaScript, Ruby, Rails, React, Redux. seeking
+            growth and open to new challenges
+          </p>
         </section>
+        <Socials />
       </main>
     </>
   );
