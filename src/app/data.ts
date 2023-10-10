0da2a59763cfd84683d665843404800e51f3e158
Pablo <ezekielgonzalezr@gmail.com>
2023-10-09 20:39:41 -0600

    Create file to store projects data

---
+++ src/app/data.ts
@@ -0,0 +1,88 @@
+const projects = [
+  {
+    name: 'December Countdown',
+    description:
+      'A December and Christmas countdown with anomations of snow falling',
+    paragraph:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image:
+      'https://th.bing.com/th/id/R.607c3509120ebcc18ee155a6e9336426?rik=0xTRelDI8j54Qw&pid=ImgRaw&r=0',
+    techs: ['HTML', 'Css', 'JavaScript'],
+    live: 'https://decembercountdown.netlify.app/',
+    repo: 'https://github.com/PabloEGonz/decemberCountdown',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    paragraph:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image:
+      'https://th.bing.com/th/id/R.607c3509120ebcc18ee155a6e9336426?rik=0xTRelDI8j54Qw&pid=ImgRaw&r=0',
+    techs: ['HTML', 'Css', 'Bootstrap'],
+    live: '',
+    repo: '#',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    paragraph:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image:
+      'https://th.bing.com/th/id/R.607c3509120ebcc18ee155a6e9336426?rik=0xTRelDI8j54Qw&pid=ImgRaw&r=0',
+    techs: ['HTML', 'Bootstrap', 'Ruby'],
+    live: '#',
+    repo: '#',
+  },
+  {
+    name: 'Project 4',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    paragraph:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image:
+      'https://th.bing.com/th/id/R.607c3509120ebcc18ee155a6e9336426?rik=0xTRelDI8j54Qw&pid=ImgRaw&r=0',
+    techs: ['HTML', 'Bootstrap', 'Ruby'],
+    live: '#',
+    repo: '#',
+  },
+  {
+    name: 'Project 5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    paragraph:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image:
+      'https://th.bing.com/th/id/R.607c3509120ebcc18ee155a6e9336426?rik=0xTRelDI8j54Qw&pid=ImgRaw&r=0',
+    techs: ['HTML', 'Bootstrap', 'Ruby'],
+    live: '#',
+    repo: '#',
+  },
+  {
+    name: 'Project 6',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    paragraph:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image:
+      'https://th.bing.com/th/id/R.607c3509120ebcc18ee155a6e9336426?rik=0xTRelDI8j54Qw&pid=ImgRaw&r=0',
+    techs: ['HTML', 'Bootstrap', 'Ruby'],
+    live: '#',
+    repo: '#',
+  },
+  {
+    name: 'Project 7',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    paragraph:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image:
+      'https://th.bing.com/th/id/R.607c3509120ebcc18ee155a6e9336426?rik=0xTRelDI8j54Qw&pid=ImgRaw&r=0',
+    techs: ['HTML', 'Bootstrap', 'Ruby'],
+    live: '#',
+    repo: '#',
+  },
+];
+
+export default projects;
