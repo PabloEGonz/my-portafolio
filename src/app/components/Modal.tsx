5b7e915ef0fed92538e83c74cedd567b9ea4db97
Pablo <ezekielgonzalezr@gmail.com>
2023-10-13 10:46:57 -0600

    Allow popup to be close by escape

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import ReactPortal from './ReactPortal';
 interface ModalProps {
   children: ReactNode;
@@ -8,15 +8,31 @@ interface ModalProps {
 }
 
 const Modal: NextPage<ModalProps> = ({ children, isOpen, handleClose }) => {
+  useEffect( () => {
+    const closeOnEscape = (e: KeyboardEvent) => 
+    e.key === 'Escape' ? handleClose() : null;
+    document.body.addEventListener('keydown', closeOnEscape);
+    return () => {
+      document.body.removeEventListener('keydown', closeOnEscape);
+    };
+  }, [handleClose]);
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return (): void => {
+      document.body.style.overflow = 'unset';
+    }
+  }, [isOpen])
+
   if (!isOpen) return null;
 
   return (
     <ReactPortal wrapperId='portal-modal-container'>
       <>
-        <div className='fixed left-0 top-0 z-40 h-screen w-screen bg-neutral-800 opacity-50'>
+        <div className='fixed left-0 top-0 z-40 flex h-screen w-screen justify-center bg-black text-center opacity-80'>
           <div
             id='my_modal_3'
-            className='bg-zinc-800 fixed inset-x-96 inset-y-32 box-border flex min-w-fit flex-col overflow-hidden rounded p-5'
+            className='bg-zinc-800 fixed inset-y-48 box-border flex min-w-fit flex-col overflow-hidden rounded p-5'
           >
             <div className='modal-box'>
               <form method='dialog'>
