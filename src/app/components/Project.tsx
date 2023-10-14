4dc9ed809b2fa7ef84283bc08a0edf3ef901e39c
Pablo <ezekielgonzalezr@gmail.com>
2023-10-13 21:09:30 -0600

    Add project description to cards

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -26,7 +26,7 @@ const Project: NextPage<ProjectProps> = ({ data }) => {
       </figure>
       <div className='card-body'>
         <h2 className='card-title'>{data.name}</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <p>{data.description}</p>
         <div className='card-actions justify-end'>
           <button className='btn' onClick={() => setIsOpen(true)}>
             open modal
