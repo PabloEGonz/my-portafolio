ced4727cb708a7100834251d1c745c5b5a1a4f8e
Pablo <ezekielgonzalezr@gmail.com>
2023-10-14 21:38:58 -0600

    Style popup window

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,8 +1,8 @@
 import { NextPage } from 'next';
-import React, { ReactNode }  from 'react';
+import React, { ReactNode } from 'react';
 import ReactPortal from './ReactPortal';
 interface ModalProps {
-  children: ReactNode
+  children: ReactNode;
   isOpen: boolean;
   handleClose: () => void;
 }
@@ -12,19 +12,26 @@ const Modal: NextPage<ModalProps> = ({ children, isOpen, handleClose }) => {
 
   return (
     <ReactPortal wrapperId='portal-modal-container'>
-      <div id='my_modal_3' className='modal z-40 bg-black'>
-        <div className='modal-box'>
-          <form method='dialog'>
-            <button
-              className='btn btn-sm btn-circle btn-ghost absolute right-2 top-2'
-              onClick={handleClose}
-            >
-              ✕
-            </button>
-          </form>
-          <div>{children}</div>
+      <>
+        <div className='fixed left-0 top-0 z-40 h-screen w-screen bg-neutral-800 opacity-50'>
+          <div
+            id='my_modal_3'
+            className='bg-zinc-800 fixed inset-x-96 inset-y-32 box-border flex min-w-fit flex-col overflow-hidden rounded p-5'
+          >
+            <div className='modal-box'>
+              <form method='dialog'>
+                <button
+                  className='btn btn-circle btn-ghost btn-sm absolute right-2 top-2'
+                  onClick={handleClose}
+                >
+                  ✕
+                </button>
+              </form>
+              <div>{children}</div>
+            </div>
+          </div>
         </div>
-      </div>
+      </>
     </ReactPortal>
   );
 };
--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -2,7 +2,7 @@
 import { NextPage } from 'next';
 import Modal from './Modal';
 import React, { useState } from 'react';
-import Image,{ StaticImageData }  from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 
 interface ProjectProps {
   data: {
@@ -17,12 +17,12 @@ interface ProjectProps {
 }
 
 const Project: NextPage<ProjectProps> = ({ data }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   return (
-    <div className='card card-compact w-96 bg-base-100 shadow-xl'>
+    <div className='card-compact card w-96 bg-base-100 shadow-xl'>
       <figure>
-        <Image src={data.image} alt='project image'/>
+        <Image src={data.image} alt='project image' />
       </figure>
       <div className='card-body'>
         <h2 className='card-title'>{data.name}</h2>
@@ -33,12 +33,10 @@ const Project: NextPage<ProjectProps> = ({ data }) => {
           </button>
           {isOpen && (
             <Modal handleClose={() => setIsOpen(false)} isOpen={isOpen}>
-              <div>
-                <h3 className='text-lg font-bold'>Hello!</h3>
-                <p className='py-4'>
-                  Press ESC key or click on ✕ button to close
-                </p>
-              </div>
+              <h3 className='text-lg font-bold'>Hello!</h3>
+              <p className='py-4'>
+                Press ESC key or click on ✕ button to close
+              </p>
             </Modal>
           )}
         </div>
