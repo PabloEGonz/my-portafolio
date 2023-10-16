4879a1398f88215bb2bdb9e674635ef96a1d6b29
Pablo <ezekielgonzalezr@gmail.com>
2023-10-15 20:42:04 -0600

    Improve contact section styling

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,3 +1,4 @@
+'use-client';
 import { NextPage } from 'next';
 
 interface Props {}
@@ -5,16 +6,28 @@ interface Props {}
 const Contact: NextPage<Props> = ({}) => {
   return (
     <section className='flex min-h-screen p-52'>
-      <h3 className='w-1/3 text-3xl'>
+      <h3 className='w-1/3 text-3xl text-secondary'>
         I'm always interested in hearing about new projects, so if you would
         like to get in contact with me.
       </h3>
-      <form action='post' className='flex flex-col gap-5 p-10'>
-        <input type='text' placeholder='Name' />
-        <input type='text' placeholder='Email' />
-        <textarea name='comments' id='comments' cols={30} rows={10}>
-          Write anything
+      <form
+        action='https://formspree.io/f/xwkjrpwp'
+        method='post'
+        className='flex w-2/3 flex-col gap-5 px-20'
+      >
+        <input
+          type='text'
+          placeholder='Name'
+          className='input input-bordered'
+        />
+        <input
+          type='text'
+          placeholder='Email'
+          className='input input-bordered'
+        />
+        <textarea name='comments' id='comments' cols={30} rows={5} placeholder='Write me Something' className='textarea textarea-bordered'>
         </textarea>
+        <button type='submit' className='btn bg-purple w-1/6 hover:bg-pink'>Submit</button>
       </form>
     </section>
   );
