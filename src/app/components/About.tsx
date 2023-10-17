ff72dc86fef56f2734a38875770954733646e69b
Pablo <ezekielgonzalezr@gmail.com>
2023-10-16 20:41:27 -0600

    Improve about section styling

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -4,14 +4,16 @@ interface Props {}
 
 const About: NextPage<Props> = ({}) => {
   return (
-    <section className='p-52 min-h-screen'>
+    <section className='min-h-screen p-52 text-secondary'>
       <h2 className='text-6xl'>About me</h2>
-      <p className='text-lg pr-64 py-10'>
+      <p className='py-10 pr-64 text-2xl font-thin text-primary'>
         Hello I’m a software developer! I can help you build a product , feature
         or website Look through some of my work and experience! If you like what
         you see and have a project you need coded, don’t hesitate to contact me.
       </p>
-      <button type='button' className='btn bg-purple text-white'>Get my resume</button>
+      <button type='button' className='hover:bg-pink btn bg-purple text-white'>
+        Get my resume
+      </button>
     </section>
   );
 };
