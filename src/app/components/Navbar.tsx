4a4e3e4538f504075a73e15768e78197e04ef12f
Pablo <ezekielgonzalezr@gmail.com>
2023-10-16 20:41:07 -0600

    Improve navbar styling

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ interface Props {}
 
 const Navbar: NextPage<Props> = ({}) => {
   return (
-    <nav className='fixed inset-x-0  left-0 top-0 z-10 bg-black p-6'>
+    <nav className='fixed inset-x-0  left-0 top-0 z-10 border-b border-secondary bg-black p-6'>
       <ul className='flex  w-4/6 gap-12 '>
         <li className='text-primary hover:text-secondary'>About</li>
         <li className='text-primary hover:text-secondary'>Home</li>
