f7ab0277241811695513d7f2788f9fbc644aa199
Pablo <ezekielgonzalezr@gmail.com>
2023-10-18 12:17:31 -0600

    Add links to section of webpage

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,12 +4,59 @@ interface Props {}
 
 const Navbar: NextPage<Props> = ({}) => {
   return (
-    <nav className='fixed inset-x-0  left-0 top-0 z-10 border-b border-secondary bg-black p-6'>
-      <ul className='flex  w-4/6 gap-12 '>
-        <li className='text-primary hover:text-secondary'>About</li>
-        <li className='text-primary hover:text-secondary'>Home</li>
-        <li className='text-primary hover:text-secondary'>Projects</li>
-      </ul>
+    <nav className='navbar fixed inset-x-0  left-0 top-0 z-10 border-b border-secondary bg-black p-6'>
+      <div className='navbar-start lg:hidden'>
+        <div className='dropdown '>
+          <label tabIndex={0} className='btn btn-circle btn-ghost text-white'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              className='inline-block h-5 w-5 stroke-current'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M4 6h16M4 12h16M4 18h16'
+              ></path>
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className='menu dropdown-content rounded-box z-[1] mt-3 w-52 bg-black p-2 shadow'
+          >
+            <li className='text-primary hover:text-secondary'>
+              <a href='#home'>Home</a>
+            </li>
+            <li className='text-primary hover:text-secondary'>
+              <a href='#projects'>Projects</a>
+            </li>
+            <li className='text-primary hover:text-secondary'>
+              <a href='#about'>About</a>
+            </li>
+            <li className='text-primary hover:text-secondary'>
+              <a href='#contact'>Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <ul className='hidden w-4/6  gap-12 lg:flex '>
+          <li className='text-primary hover:text-secondary'>
+            <a href='#home'>Home</a>
+          </li>
+          <li className='text-primary hover:text-secondary'>
+            <a href='#projects'>Projects</a>
+          </li>
+          <li className='text-primary hover:text-secondary'>
+            <a href='#about'>About</a>
+          </li>
+          <li className='text-primary hover:text-secondary'>
+            <a href='#contact'>Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
