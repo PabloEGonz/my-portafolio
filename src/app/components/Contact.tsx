1f123b45b69bfbb14ea77ed8bfd26f507c1b6792
Pablo <ezekielgonzalezr@gmail.com>
2023-10-19 12:21:03 -0600

    Add phone version for contact section

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -5,15 +5,18 @@ interface Props {}
 
 const Contact: NextPage<Props> = ({}) => {
   return (
-    <section className='flex min-h-screen p-52'>
-      <h3 className='w-1/3 text-3xl text-secondary'>
+    <section
+      id='contact'
+      className='flex min-h-screen flex-col lg:flex-row lg:p-52'
+    >
+      <h3 className='w-1/1  p-10 text-3xl text-secondary lg:w-1/3 lg:p-0'>
         I'm always interested in hearing about new projects, so if you would
         like to get in contact with me.
       </h3>
       <form
         action='https://formspree.io/f/xwkjrpwp'
         method='post'
-        className='flex w-2/3 flex-col gap-5 px-20'
+        className='flex flex-col gap-5 lg:px-20 px-10 lg:w-2/3'
       >
         <input
           type='text'
@@ -25,9 +28,17 @@ const Contact: NextPage<Props> = ({}) => {
           placeholder='Email'
           className='input input-bordered'
         />
-        <textarea name='comments' id='comments' cols={30} rows={5} placeholder='Write me Something' className='textarea textarea-bordered'>
-        </textarea>
-        <button type='submit' className='btn bg-purple w-1/6 hover:bg-pink'>Submit</button>
+        <textarea
+          name='comments'
+          id='comments'
+          cols={30}
+          rows={5}
+          placeholder='Write me Something'
+          className='textarea textarea-bordered'
+        ></textarea>
+        <button type='submit' className='btn bg-purple hover:bg-pink lg:w-1/6'>
+          Submit
+        </button>
       </form>
     </section>
   );
