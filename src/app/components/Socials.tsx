1901ad60e1c9ea3ae11f6f6e453fe75530998014
Pablo <ezekielgonzalezr@gmail.com>
2023-10-20 12:21:44 -0600

    Add phone version for socials  section

--- src/app/components/Socials.tsx
+++ src/app/components/Socials.tsx
@@ -2,34 +2,34 @@ import { NextPage } from 'next';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import { SiWellfound, SiGmail } from 'react-icons/si';
 
-interface Props {}
+interface Props {
+  direction: String;
+}
 
-const Socials: NextPage<Props> = ({}) => {
+const Socials: NextPage<Props> = ({ direction }) => {
   return (
-    <div className='bg-purple rounded fixed left-0'>
-      <ul className='flex-col p-2'>
-        <li className='m-4 text-2xl text-white hover:text-black hover:scale-110'>
-          <a href='https://github.com/PabloEGonz'>
-            <BsGithub />
-          </a>
-        </li>
-        <li className='m-4 text-2xl text-white hover:text-black hover:scale-110'>
-          <a href='https://www.linkedin.com/in/pabloezekielgonz/'>
-            <BsLinkedin />
-          </a>
-        </li>
-        <li className='m-4 text-3xl text-white hover:text-black hover:scale-110'>
-          <a href='https://wellfound.com/u/pabloezekielgonz'>
-            <SiWellfound />
-          </a>
-        </li>
-        <li className='m-4 text-2xl text-white hover:text-black hover:scale-110'>
-          <a href='mailto:ezekielgonzalezr@gmail.com'>
-            <SiGmail />
-          </a>
-        </li>
-      </ul>
-    </div>
+    <ul className={`flex flex-${direction} p-2 justify-center`}>
+      <li className='m-4 text-2xl text-white hover:scale-110 hover:text-black'>
+        <a href='https://github.com/PabloEGonz'>
+          <BsGithub />
+        </a>
+      </li>
+      <li className='m-4 text-2xl text-white hover:scale-110 hover:text-black'>
+        <a href='https://www.linkedin.com/in/pabloezekielgonz/'>
+          <BsLinkedin />
+        </a>
+      </li>
+      <li className='m-4 text-3xl text-white hover:scale-110 hover:text-black'>
+        <a href='https://wellfound.com/u/pabloezekielgonz'>
+          <SiWellfound />
+        </a>
+      </li>
+      <li className='m-4 text-2xl text-white hover:scale-110 hover:text-black'>
+        <a href='mailto:ezekielgonzalezr@gmail.com'>
+          <SiGmail />
+        </a>
+      </li>
+    </ul>
   );
 };
 
