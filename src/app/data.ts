374c5af39a99185451bb9ebb8398c5586c2c3359
Pablo <ezekielgonzalezr@gmail.com>
2023-10-23 21:00:50 -0600

    Update project data

--- src/app/data.ts
+++ src/app/data.ts
@@ -12,7 +12,7 @@ const projects = [
     paragraph:
       'This full-stack web application streamlines the process, allowing users to browse a curated list of available cars, access detailed car information, make reservations, add or remove car listings, and effortlessly manage reservations.',
     image: rentCar,
-    techs: ['Ruby on Rails', 'React','Bootstrap', 'Jsx'],
+    techs: ['Ruby on Rails', 'React', 'Bootstrap', 'JSX'],
     live: 'https://rent-a-car-front-end.onrender.com/',
     repo: 'https://github.com/PabloEGonz/full-stack-capstone-front-end',
   },
@@ -21,9 +21,9 @@ const projects = [
     description:
       'This is a mobile web application where you can manage your budget.',
     paragraph:
-      "This user-friendly mobile web application takes the hassle out of budget management. By categorizing transactions, it provides a comprehensive view of your spending habits, empowering you to make more informed financial decisions. Whether you're tracking monthly expenses, managing personal spending, or planning for savings, this app puts the power of financial control at your fingertips.",
+      "This user-friendly mobile web application takes the hassle out of budget management. By categorizing transactions, it provides a comprehensive view of your spending habits, empowering you to make more informed financial decisions. Whether you're tracking monthly expenses, managing personal spending, or planning for savings.",
     image: airQuality,
-    techs: ['HTML', 'Css', 'Bootstrap'],
+    techs: ['Ruby on Rails', 'ERB', 'CSS3', 'Bootstrap'],
     live: 'https://budget-app-13xl.onrender.com/',
     repo: 'https://github.com/PabloEGonz/bugget-app',
   },
@@ -34,18 +34,17 @@ const projects = [
     paragraph:
       'This web app provides users with an easy way to monitor air quality in different cities. Users can view air quality information for each city, add new cities, remove cities, and learn about the parameters used to determine air quality levels.',
     image: airQuality,
-    techs: ['React', 'Jsx', 'Bootstrap', 'HTML', 'CSS'],
+    techs: ['React', 'JSX', 'Bootstrap', 'CSS3'],
     live: 'https://air-quality-app-r9g3.onrender.com/',
     repo: 'https://github.com/PabloEGonz/air-quality-app',
   },
   {
     name: 'Chinese Zodiac Animal',
-    description:
-      'Web app to discover what zodiac animal you are.',
+    description: 'Web app to discover what zodiac animal you are.',
     paragraph:
       'This is a webpage that tells you what Chinese zodiac sign you are, you have to enter your date of birth and it will show your zodiac animal.',
     image: chineseZodiac,
-    techs: ['Vite', 'TypeScript', 'Bootstrap', 'SCCS', 'HTML'],
+    techs: ['Vite', 'TypeScript', 'Bootstrap', 'SCCS'],
     live: 'https://knowyourchinesezodiacanimal.netlify.app/',
     repo: 'https://github.com/PabloEGonz/chineseZodiac',
   },
@@ -54,9 +53,9 @@ const projects = [
     description:
       'This is a calculator web application that uses an API to display quotes.',
     paragraph:
-      "Math Magicians is a web app designed using React, a popular JavaScript library for building user interfaces, the calculator offers a clean and intuitive interface. It supports basic arithmetic operations such as addition, subtraction, multiplication, and division, allowing users to perform calculations with ease. The React framework's component-based architecture ensures that the calculator is highly modular, making it easy to extend or customize its functionality. Additionally, the calculator incorporates responsive design principles, ensuring a seamless experience across various.",
+      'Math Magicians is a web app designed using React, the calculator offers a clean and intuitive interface. It supports basic arithmetic operations such as addition, subtraction, multiplication, and division, allowing users to perform calculations with ease. Additionally, the calculator incorporates responsive design principles, ensuring a seamless experience across various.',
     image: calculator,
-    techs: ['React', 'Jsx', 'Bootstrap', 'CSS', 'HTML'],
+    techs: ['React', 'JSX', 'Bootstrap', 'CSS3'],
     live: 'https://pabloegonz.github.io/math-magicians/',
     repo: 'https://github.com/PabloEGonz/math-magicians',
   },
@@ -65,9 +64,9 @@ const projects = [
     description:
       'A December and Christmas countdown with anomations of snow falling.',
     paragraph:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+      'A handy tool to keep track of the days leading up to December. Get excited as you watch the countdown timer ticking closer to the holiday season.',
     image: xmas,
-    techs: ['HTML', 'Css', 'JavaScript'],
+    techs: ['HTML', 'CSS3', 'JavaScript'],
     live: 'https://decembercountdown.netlify.app/',
     repo: 'https://github.com/PabloEGonz/decemberCountdown',
   },
