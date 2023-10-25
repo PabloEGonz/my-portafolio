7bb6d7be58cb82b43f75d7a2cb02c3485ec3908b
Pablo <ezekielgonzalezr@gmail.com>
2023-10-24 21:26:02 -0600

    Update project live link

--- src/app/data.ts
+++ src/app/data.ts
@@ -25,7 +25,7 @@ const projects = [
       "This user-friendly mobile web application takes the hassle out of budget management. By categorizing transactions, it provides a comprehensive view of your spending habits, empowering you to make more informed financial decisions. Whether you're tracking monthly expenses, managing personal spending, or planning for savings.",
     image: budget,
     techs: ['Ruby on Rails', 'ERB', 'CSS3', 'Bootstrap'],
-    live: 'https://budget-app-13xl.onrender.com/',
+    live: 'https://budget-app-x025.onrender.com/',
     repo: 'https://github.com/PabloEGonz/bugget-app',
   },
   {
