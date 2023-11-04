d81d588bc26cbcb23c29a802b7b987121e8f79d4
Pablo <ezekielgonzalezr@gmail.com>
2023-11-03 22:31:44 -0600

    Update resume link

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -4,16 +4,19 @@ interface Props {}
 
 const About: NextPage<Props> = ({}) => {
   return (
-    <section id='about' className='text-secondary lg:p-52 p-10'>
+    <section id='about' className='p-10 text-secondary lg:p-52'>
       <h2 className='text-6xl'>About me</h2>
-      <p className='text-2xl font-thin text-primary py-10 lg:pr-64'>
+      <p className='py-10 text-2xl font-thin text-primary lg:pr-64'>
         Hello I’m a software developer! I can help you build a product , feature
         or website Look through some of my work and experience! If you like what
         you see and have a project you need coded, don’t hesitate to contact me.
       </p>
-      <button type='button' className='btn bg-purple text-white hover:bg-pink'>
+      <a
+        href='https://docs.google.com/document/d/1vDG_LiYK4fE7tvUjZlCVZRz3X1YyGbd6hbugheDhOWs/edit'
+        className='btn bg-purple text-white hover:bg-pink'
+      >
         Get my resume
-      </button>
+      </a>
     </section>
   );
 };
