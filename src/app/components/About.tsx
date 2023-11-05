34b039f8c2b4e549756282523efe789315d6c9a2
Pablo <ezekielgonzalezr@gmail.com>
2023-11-04 21:36:13 -0600

    Update about paragraph

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -7,9 +7,11 @@ const About: NextPage<Props> = ({}) => {
     <section id='about' className='p-10 text-secondary lg:p-52'>
       <h2 className='text-6xl'>About me</h2>
       <p className='py-10 text-2xl font-thin text-primary lg:pr-64'>
-        Hello I’m a software developer! I can help you build a product , feature
-        or website Look through some of my work and experience! If you like what
-        you see and have a project you need coded, don’t hesitate to contact me.
+        Hello, I'm Pablo, a skilled software developer ready to assist you in
+        building your next product, feature, or website. Please take a moment to
+        explore my portfolio and experience. If you're impressed with what you
+        find and have a project in mind that needs coding, feel free to reach
+        out to me. I'm here to help bring your ideas to life!
       </p>
       <a
         href='https://docs.google.com/document/d/1vDG_LiYK4fE7tvUjZlCVZRz3X1YyGbd6hbugheDhOWs/edit'
