57b073f654fa9132b4c9f8bd4153443aa7172dfc
Pablo <ezekielgonzalezr@gmail.com>
2023-11-23 07:01:54 -0600

    Improve catet color on dark them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const rubik = Rubik({
   subsets: ['latin'],
 });
 const type =
-  'lg:relative lg:w-max lg:before:absolute lg:before:inset-0 lg:before:h-20 lg:before:animate-typewriter before:bg-white dark:before:bg-black  lg:after:absolute lg:after:inset-0 lg:after:w-0.5 lg:after:w-1 lg:after:animate-caret lg:after:bg-black';
+  'lg:relative lg:w-max lg:before:absolute lg:before:inset-0 lg:before:h-20 lg:before:animate-typewriter before:bg-white dark:before:bg-black  lg:after:absolute lg:after:inset-0 lg:after:w-0.5 lg:after:w-1 lg:after:animate-caret lg:after:bg-black dark:after:bg-white';
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-between dark:bg-black'>
