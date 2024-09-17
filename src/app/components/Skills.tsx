7464e3614d6f4f5e5d48ab5618261ea1a17b5edc
Pablo <ezekielgonzalezr@gmail.com>
2024-09-16 20:45:23 -0600

    Fix overflow issue

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -4,7 +4,7 @@ interface Props {}
 
 const Skills: NextPage<Props> = ({}) => {
   return (
-    <div className='relative h-[120vh] min-h-[100vh] overflow-hidden rounded-3xl md:h-[100vh]'>
+    <div className='relative h-[120vh] min-h-[100vh] overflow-hidden w-full rounded-3xl md:h-[100vh]'>
       <BackgroundGradient />
       <div className='absolute left-0 top-0 flex h-full w-full items-center justify-center'>
         <section className='grid grid-cols-1 gap-4 pb-20 md:grid-cols-3 lg:gap-40'>
@@ -12,7 +12,7 @@ const Skills: NextPage<Props> = ({}) => {
             <h5 className='mb-3 text-3xl font-bold text-black drop-shadow-lg'>
               Languages
             </h5>
-            <ul className='grid gap-1'>
+            <ul className='grid gap-1 md:gap-1.5'>
               <li className='rounded bg-white/30 p-2 text-sm text-black shadow-2xl backdrop-blur-xl transition-colors duration-700 hover:bg-black/40 hover:text-white md:text-lg'>
                 JavaScript
               </li>
@@ -34,7 +34,7 @@ const Skills: NextPage<Props> = ({}) => {
             <h5 className='mb-3 text-3xl font-bold text-black drop-shadow-lg'>
               Frameworks
             </h5>
-            <ul className='grid gap-1'>
+            <ul className='grid gap-1 md:gap-1.5'>
               <li className='rounded bg-white/30 p-2 text-sm text-black shadow-2xl backdrop-blur-xl transition-colors duration-700 hover:bg-black/40 hover:text-white md:text-lg'>
                 React JS
               </li>
@@ -53,7 +53,7 @@ const Skills: NextPage<Props> = ({}) => {
             <h5 className='mb-3 text-3xl font-bold text-black drop-shadow-lg'>
               Skills{' '}
             </h5>
-            <ul className='grid gap-1'>
+            <ul className='grid gap-1 md:gap-1.5'>
               <li className='rounded bg-white/30 p-2 text-sm text-black shadow-2xl backdrop-blur-xl transition-colors duration-700 hover:bg-black/40 hover:text-white md:text-lg'>
                 Data Mangement
               </li>
