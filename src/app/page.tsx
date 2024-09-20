dbde5e98de4a8ac10125ab23d5880f7c3c331198
Pablo <ezekielgonzalezr@gmail.com>
2024-09-19 20:34:55 -0600

    Refactor component imports and file structure, and add Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,15 @@
 import About from '../components/About';
 import Contact from '../components/Contact';
 import Skills from '../components/Skills';
-import { Rubik } from 'next/font/google';
-import ThreeAnimate from '../components/ThreeAnimate';
 import Timeline from '../components/projects/Timeline';
+import Hero from '@/components/hero/Hero';
 
-const rubik = Rubik({
-  weight: ['300', '400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'],
-});
 const type =
   'lg:relative lg:w-max lg:before:absolute lg:before:inset-0 lg:before:h-20 lg:before:animate-typewriter before:bg-white dark:before:bg-black  lg:after:absolute lg:after:inset-0 lg:after:w-0.5 lg:after:w-1 lg:after:animate-caret lg:after:bg-black dark:after:bg-white';
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-between'>
-      <section id='home' className='hero min-h-screen overflow-hidden'>
-        <ThreeAnimate />
-        <div className='hero-content text-center'>
-          <div className='max-w-md'>
-            <h1 className='animate-gradient mb-5 bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-violet-600 via-indigo-600  via-sky-400  via-teal-500 to-pink bg-clip-text pt-16 text-7xl text-primary text-transparent duration-300'>
-              Hey there I&apos;m Ezequiel
-            </h1>
-            <h2 className='mb-6 rounded-lg bg-white/30 p-3 text-4xl text-black backdrop-blur-md'>
-              I&apos;m a full-stack software developer
-            </h2>
-            <p
-              className={
-                rubik.className +
-                ' rounded-lg bg-white/50 py-6 font-sans text-lg font-light backdrop-blur-md'
-              }
-            >
-              Certified Full-stack web developer with experience in
-              collaborative environments, persistent in overcoming obstacles
-              with a positive mindset. Constantly seeking new learning
-              opportunities and demonstrating a strong determination to achieve
-              set goals. Skilled in building web applications using Ruby on
-              Rails, React, and Next.js.
-            </p>
-            <div className='rounded bg-purple lg:hidden'>
-            </div>
-          </div>
-        </div>
-        <div className='left-0 z-10 hidden  rounded-e-lg bg-purple lg:fixed lg:block'>
-        </div>
-      </section>
+      <Hero />
       <Timeline />
       <About />
       <Skills />
---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,28 @@
+import ThreeAnimate from '../ThreeAnimate';
+import { Rubik } from 'next/font/google';
+import ProfileCard from './ProfileCard';
+import ProfileCardMobile from './ProfileCardMobile';
+
+const rubik = Rubik({
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+});
+
+const Hero = () => {
+  return (
+    <section id='home' className='hero min-h-screen overflow-hidden'>
+      <ThreeAnimate />
+      <div className='hero-content text-center'>
+        <div className='max-w-md'>
+          <h1 className='animate-gradient mb-5 bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-violet-600 via-indigo-600  via-sky-400  via-teal-500 to-pink bg-clip-text pt-16 text-7xl text-primary text-transparent duration-300'>
+            Hey there I&apos;m Ezequiel
+          </h1>
+          <ProfileCard />
+          <ProfileCardMobile />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
