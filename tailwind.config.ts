e4180bd8b2b63d4c889a237be3a11aa279549425
Pablo <ezekielgonzalezr@gmail.com>
2025-05-07 13:52:08 -0600

    Update black color in Tailwind config to pure black (#000000)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        black: '#212529',
+        black: '#000000',
         'text-white': '#fff',
         white: '#f8f9fa',
         primary: '#343a40',
