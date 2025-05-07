347ea9fc167df5e475acd2c4f1c15fb8f2c9a8b6
Pablo <ezekielgonzalezr@gmail.com>
2025-05-07 15:34:35 -0600

    Update Navbar styles for improved visibility and consistency

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,19 +6,19 @@ const Navbar = ({}) => {
   return (
     <Nav
       shouldHideOnScroll
-      className='z-30 bg-white/50 py-6 backdrop-blur-3xl lg:p-6'
+      className='z-40 bg-black py-6 lg:p-6'
     >
       <div className='navbar-start w-full lg:hidden'>
         <div className='dropdown'>
           <label
             tabIndex={0}
-            className='border-1 btn btn-ghost border-black bg-black text-white'
+            className='border-1 btn-ghost border-black bg-black text-white'
           >
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
               viewBox='0 0 24 24'
-              className='inline-block h-5 w-5 stroke-current'
+              className='inline-block h-6 w-6 stroke-current'
             >
               <path
                 strokeLinecap='round'
@@ -30,7 +30,7 @@ const Navbar = ({}) => {
           </label>
           <ul
             tabIndex={0}
-            className='menu gap-3 dropdown-content rounded-box z-[1] mt-3 w-52 bg-white/95 p-2 text-lg font-light text-black shadow backdrop-blur-3xl'
+            className='menu gap-3 dropdown-content rounded z-[1] mt-3 w-52 bg-white/95 p-4 text-lg font-light text-black shadow backdrop-blur-3xl'
           >
             <Link href='/'>Home</Link>
             <Link href='#projects'>Projects</Link>
