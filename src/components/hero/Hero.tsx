e36971e08c2365cf21be7602786d7b3bcb487724
Pablo <ezekielgonzalezr@gmail.com>
2025-05-07 15:36:09 -0600

    Update Hero component heading size for better responsiveness

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <ThreeAnimate />
       <div className='hero-content text-center'>
         <div className='max-w-md'>
-          <h1 className='animate-gradient mb-5 text-7xl font-semibold text-primary bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-violet-600 via-indigo-600  via-sky-400  via-teal-500 to-pink bg-clip-text text-transparent duration-300'>
+          <h1 className='animate-gradient mb-5 text-5xl md:text-7xl font-semibold text-primary bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-violet-600 via-indigo-600  via-sky-400  via-teal-500 to-pink bg-clip-text text-transparent duration-300'>
             Hey there I&apos;m Ezequiel
           </h1>
           <ProfileCard />
