d7ffcc3ff0c6ecc2b88e632c5242b0971d39f6ed
Pablo <ezekielgonzalezr@gmail.com>
2025-05-07 18:53:17 -0600

    Refactor Skills component for improved readability and layout consistency

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -6,7 +6,14 @@ interface Props {}
 
 const frontEnd = ['React', 'Next.js', 'Vite', 'HTML', 'CSS', 'SASS'];
 const backEnd = ['Ruby on Rails', 'PostgreSQL', 'Ruby', 'MongoDB', 'Node.js'];
-const libraries = ['Axios', 'Redux Toolkit', 'Tailwind CSS', 'Bootstrap', 'Three.js', 'Framer Motion'];
+const libraries = [
+  'Axios',
+  'Redux Toolkit',
+  'Tailwind CSS',
+  'Bootstrap',
+  'Three.js',
+  'Framer Motion',
+];
 const testing = ['Capybara', 'Selenium', 'RSpec', 'Jest'];
 const tools = [
   'Git & GitHub',
@@ -19,11 +26,13 @@ const tools = [
 
 const Skills: NextPage<Props> = ({}) => {
   return (
-    <div className='relative w-full overflow-hidden rounded-3xl h-[120vh] lg:h-[70vh]'>
-      <BackgroundGradient />
-      <div className='absolute left-0 top-0 flex h-full w-full items-center justify-center'>
-        <section className='flex flex-row flex-wrap flex-shrink gap-6 pb-10 lg:gap-20 justify-center'>
-          <div className='basis-1/3 md:basis-1/4 lg:basis-auto order-first'>
+    <div className='relative w-full py-16 md:py-20 overflow-hidden rounded-3xl'>
+      <div className='absolute left-0 top-0 h-full w-full'>
+        <BackgroundGradient />
+      </div>
+      <div className='flex items-center justify-center'>
+        <section className='flex flex-shrink flex-row flex-wrap justify-center gap-10 pb-10 lg:gap-20'>
+          <div className='order-first basis-1/3 md:basis-1/4 lg:basis-auto'>
             <h5 className='mb-3 text-3xl font-bold text-black drop-shadow-lg'>
               Frontend
             </h5>
@@ -43,7 +52,7 @@ const Skills: NextPage<Props> = ({}) => {
               ))}
             </ul>
           </div>
-          <div className='basis-1/3 md:basis-1/4 lg:basis-auto order-first lg:order-last'>
+          <div className='order-first basis-1/3 md:basis-1/4 lg:order-last lg:basis-auto'>
             <h5 className='mb-3 text-3xl font-bold text-black drop-shadow-lg'>
               Libraries
             </h5>
